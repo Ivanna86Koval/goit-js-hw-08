ce898c5af578e5fbd85252f152142659474db080
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-10 23:38:54 +0300

    update gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,25 +3,37 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
-const galleryContainer = document.querySelector('.gallery');
-const markup = galleryItems.reduce(
-  (acc, { original, preview, description }) =>
-    (acc += `<li>
-  <a class="gallery__item" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      alt="${description}"
-    />
-  </a>
-</li>`),
-  ''
-);
+/*console.log(galleryItems);*/
+const galleryRef = document.querySelector('.gallery');
 
-galleryContainer.insertAdjacentHTML('beforeend', markup);
+function createGallery(galleryItems) {
+  const galleryMarkup = galleryItems.map(createElementMarkup).join('');
 
-const lightbox = new SimpleLightbox('.gallery a', {
+  galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+}
+
+function createElementMarkup({ description, original, preview }) {
+  return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+}
+
+createGallery(galleryItems);
+
+galleryRef.addEventListener('click', onClickImg);
+
+function onClickImg(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  e.preventDefault();
+}
+
+new SimpleLightbox(`.gallery a`, {
   captionsData: 'alt',
   captionDelay: 250,
 });
