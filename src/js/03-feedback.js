82bff2d3354d1e655e82f6d290d73e1cdb285512
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-11 10:28:41 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
-const LOCAL_KEY = 'feedback-form-state';
 
+/*const LOCAL_KEY = 'feedback-form-state';
 form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onInputData, 500));
@@ -33,4 +33,42 @@ function onFormSubmit(e) {
   localStorage.removeItem(LOCAL_KEY);
   e.currentTarget.reset();
   dataForm = {};
+}*/
+
+const LOCAL_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (refs.input.value === '' || refs.textarea.value === '') {
+    return alert(`Please fill in all the fields!`);
+  }
+  or;
+  const { email, message } = e.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+  or;
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+function reloadPage() {
+  if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
 }
